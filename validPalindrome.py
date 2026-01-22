60a64608121f9e13ccfd0cbd1a1c3d813c4160ea
huscse <husnainkhaliq82@gmail.com>
2026-01-21 22:50:11 -0500

    logical solution - no code

--- validPalindrome.py
+++ validPalindrome.py
@@ -6,7 +6,6 @@ A phrase is a palindrome if, after converting all uppercase letters into lowerca
 Given a string s, return true if it is a palindrome, or false otherwise.
 
  
-
 Example 1:
 
 Input: s = "A man, a plan, a canal: Panama"
@@ -26,3 +25,15 @@ Since an empty string reads the same forward and backward, it is a palindrome.
 
 """
 
+"""
+Solution:
+ - Convert all characters in the string to lowercase
+ - Use two pointers: left and right
+ - left starts at the beginning, right starts at the end
+ - While left is less than right:
+     - If the left character is not alphanumeric, move left forward
+     - Else if the right character is not alphanumeric, move right backward
+     - Else if the characters at left and right are not equal, return False
+     - Otherwise, move both pointers inward
+ - If the loop completes, return True (the string is a palindrome)
+"""
