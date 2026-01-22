93206927cd28a32f960ed972ac1c69727e1a0877
huscse <husnainkhaliq82@gmail.com>
2026-01-21 22:52:47 -0500

    solution

--- validPalindrome.py
+++ validPalindrome.py
@@ -37,3 +37,25 @@ Solution:
      - Otherwise, move both pointers inward
  - If the loop completes, return True (the string is a palindrome)
 """
+
+def validPalindrome(s):
+    s = s.lower()
+    l = 0
+    r = len(s) - 1
+
+    while l <= r:
+        if not s[l].isalnum():
+            l += 1
+            continue
+
+        if not s[r].isalnum():
+            r -= 1
+            continue
+
+        if s[l] != s[r]:
+            return False
+        
+        l += 1
+        r -= 1
+
+    return True
