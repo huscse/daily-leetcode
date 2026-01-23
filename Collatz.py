bd268c24975903d596ee674901ff10d93506c4fd
huscse <husnainkhaliq82@gmail.com>
2026-01-22 19:30:30 -0500

    Collatz (3n + 1) Sequence

---
+++ Collatz.py
@@ -0,0 +1,27 @@
+"""
+    01 / 22 / 2026 - Collatz (3n + 1) Sequence
+
+Write a function that takes a positive integer n and repeatedly applies the following rules until n becomes 1:
+
+    - If n is even, replace n with n / 2
+
+    - If n is odd, replace n with 3n + 1
+
+Your function should return the number of steps it takes to reach 1.
+
+Requirements
+
+Input: an integer n where n >= 1
+
+Output: an integer = number of operations needed to reach 1
+
+If n is already 1, return 0
+
+Example
+
+Input: n = 6
+
+Sequence: 6 → 3 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+
+Output: 8 (it took 8 steps)
+"""
