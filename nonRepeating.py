f1571e96d02d8090557c540751548cba3e9eb7b6
huscse <husnainkhaliq82@gmail.com>
2026-01-24 18:25:35 -0500

    first non repeating character

---
+++ nonRepeating.py
@@ -0,0 +1,40 @@
+"""
+    01 / 24 / 2026 - First Non-Repeating Character (Index)
+
+Problem: First Non-Repeating Character (Index)
+
+Write a function that takes a string s and returns the index of the first character that appears only once in the string.
+
+If no such character exists, return -1.
+
+Rules
+
+Characters are case-sensitive ('a' ≠ 'A')
+
+Return the index, not the character
+
+The order of the string matters
+
+Examples
+
+Input:
+s = "leetcode"
+
+Output:
+0
+('l' appears once and is first)
+
+Input:
+s = "loveleetcode"
+
+Output:
+2
+('v' is the first non-repeating character)
+
+Input:
+s = "aabb"
+
+Output:
+-1
+
+"""
