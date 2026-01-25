eda391acfe00cd6bff82d91618e7be1d493b7f29
huscse <husnainkhaliq82@gmail.com>
2026-01-25 18:06:00 -0500

    solution

--- moveZeros.py
+++ moveZeros.py
@@ -54,3 +54,17 @@ Solution:
 
 """
 
+def moveZeros(nums):
+    pos = 0
+
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[pos] = nums[i]
+            pos += 1
+
+    for i in range(pos, len(nums)):
+        nums[i] = 0
+
+    return nums
+
+
