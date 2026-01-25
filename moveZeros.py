98777daabc7123e6f205c0c0e1c8a695014a59d5
huscse <husnainkhaliq82@gmail.com>
2026-01-25 17:51:24 -0500

    move zeros to end

---
+++ moveZeros.py
@@ -0,0 +1,37 @@
+"""
+    01 / 25 / 2026 - Move Zeros to the End
+
+Problem: Move Zeros to the End
+
+Write a function that takes an array of integers and moves all zeros to the end of the array while preserving the order of non-zero elements.
+
+You must do this in-place.
+
+Rules
+
+Maintain the relative order of non-zero elements
+
+Modify the array directly (no extra arrays)
+
+Return nothing (or return the array, depending on language)
+
+Examples
+
+Input:
+[0, 1, 0, 3, 12]
+
+Output:
+[1, 3, 12, 0, 0]
+
+Input:
+[0, 0, 0]
+
+Output:
+[0, 0, 0]
+
+Input:
+[1, 2, 3]
+
+Output:
+[1, 2, 3]
+"""
