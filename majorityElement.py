ce900d090e3b854a3d84f15ca27c3f005cd1c5ef
huscse <husnainkhaliq82@gmail.com>
2026-01-28 22:04:57 -0500

    Find majority element

---
+++ majorityElement.py
@@ -0,0 +1,29 @@
+"""
+    01 / 28 / 2026 - Find the Majority Element
+
+Write a function that takes an array of integers nums and returns the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times.
+
+You may assume that a majority element always exists.
+
+Rules
+
+Input: list of integers nums
+
+Output: the majority element
+
+Majority element appears more than half the time
+
+Examples
+
+Input:
+[3, 2, 3]
+
+Output: 3
+
+Input:
+[2, 2, 1, 1, 1, 2, 2]
+
+Output: 2
+"""
