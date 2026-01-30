c4740fa6420a471a94b4dc50c7aa3d158fd201e0
huscse <husnainkhaliq82@gmail.com>
2026-01-29 19:44:22 -0500

    Max consecutive ones

---
+++ MaxConsecOnes.py
@@ -0,0 +1,31 @@
+"""
+    01 / 29 / 2026 - Problem: Maximum Consecutive Ones
+
+Write a function that takes a binary array (only 0s and 1s) and returns the maximum number of consecutive 1s in the array.
+
+Rules
+
+Input contains only 0 and 1
+
+Consecutive means adjacent
+
+Return the maximum streak of 1s
+
+Examples
+
+Input:
+[1, 1, 0, 1, 1, 1]
+
+Output: 3
+
+Input:
+[1, 0, 1, 1, 0, 1]
+
+Output: 2
+
+Input:
+[0, 0, 0]
+
+Output: 0
+
+""" 
