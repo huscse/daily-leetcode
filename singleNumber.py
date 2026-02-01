99e47295b87c49a48ca19ae77cd24d8964d47d96
huscse <husnainkhaliq82@gmail.com>
2026-02-01 17:47:09 -0500

    Find the Single Number

---
+++ singleNumber.py
@@ -0,0 +1,31 @@
+"""
+    02 / 01 / 2026 - Find the Single Number
+
+You are given a non-empty array of integers where every element appears twice except for one.
+
+Find and return the element that appears only once.
+
+Rules
+
+Every number appears exactly twice, except one
+
+You must find the single one
+
+Order does not matter
+
+Examples
+
+Input:
+[2, 2, 1]
+
+Output: 1
+
+Input:
+[4, 1, 2, 1, 2]
+
+Output: 4
+
+Input: [1]
+
+Output: 1
+"""
