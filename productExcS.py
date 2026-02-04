4ec2e4bfdd1bda1b4b997f776209223724275249
huscse <husnainkhaliq82@gmail.com>
2026-02-03 22:26:14 -0500

    product of array except self

---
+++ productExcS.py
@@ -0,0 +1,31 @@
+"""
+    02 / 03 / 2026 - Product of Array Except Self
+
+Write a function that takes an array of integers nums and returns a new array output such that:
+
+output[i] = product of all elements in nums except nums[i]
+
+Rules
+
+You cannot use division
+
+The result array must be computed in O(n) time
+
+Try to solve it using O(1) extra space (excluding the output array)
+
+Examples
+
+Input: [1, 2, 3, 4]
+
+Output: [24, 12, 8, 6]
+
+Input: [2, 3, 4, 5]
+
+Output: [60, 40, 30, 24]
+
+Output: None
+
+Input: [5, 1, 5, 3]
+
+Output: 3
+"""
