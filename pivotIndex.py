97059f66cce8aafa7a29f10ed811dce218b47f6e
huscse <husnainkhaliq82@gmail.com>
2026-02-06 18:42:09 -0500

    problem - pivot index

---
+++ pivotIndex.py
@@ -0,0 +1,28 @@
+"""
+    02 / 06 / 2026 - Pivot Index
+
+Given an array of integers nums, return the pivot index where:
+
+The sum of all elements to the left of the index equals
+
+The sum of all elements to the right of the index
+
+If multiple pivot indices exist, return the leftmost one.
+If none exist, return -1.
+
+Examples
+
+Input:
+[1, 7, 3, 6, 5, 6]
+Output: 3
+Left sum = 1+7+3 = 11, right sum = 5+6 = 11
+
+Input:
+[1, 2, 3]
+Output: -1
+
+Input:
+[2, 1, -1]
+Output: 0
+Left sum = 0, right sum = 1 + (-1) = 0
+"""
