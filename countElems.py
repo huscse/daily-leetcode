1373a6e8650331edeaaf0a5ed65889bd0b75b559
huscse <husnainkhaliq82@gmail.com>
2026-02-06 17:19:57 -0500

    Count Elements Smaller Than the Current Number

---
+++ countElems.py
@@ -0,0 +1,27 @@
+"""
+    02 / 06 / 2026 - Count Elements Smaller Than the Current Number
+
+Given an array nums, return a new array result such that:
+
+result[i] = number of elements in nums that are smaller than nums[i]
+
+Example
+
+Input:
+[8, 1, 2, 2, 3]
+
+Output:
+[4, 0, 1, 1, 3]
+
+Explanation:
+
+8 → four numbers smaller
+
+1 → none smaller
+
+2 → one number smaller (1)
+
+2 → one number smaller
+
+3 → three numbers smaller (1,2,2)
+"""
