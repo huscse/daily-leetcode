ded821a7a9bb57ed0c9b98372b6825ac340d8a24
huscse <husnainkhaliq82@gmail.com>
2026-02-07 14:57:07 -0500

    add tree node class

--- leafNodes.py
+++ leafNodes.py
@@ -1,28 +1,20 @@
 """
     02 / 07 / 2026 - Count leaf nodes
 
-Given an array of integers nums, return the pivot index where:
+🌳 Problem 2: Count Leaf Nodes
 
-The sum of all elements to the left of the index equals
+A leaf is a node with:
 
-The sum of all elements to the right of the index
+no left child
 
-If multiple pivot indices exist, return the leftmost one.
-If none exist, return -1.
+no right child
 
-Examples
-
-Input:
-[1, 7, 3, 6, 5, 6]
-Output: 3
-Left sum = 1 + 7 + 3 = 11, right sum = 5 + 6 = 11
-
-Input:
-[1, 2, 3]
-Output: -1
-
-Input:
-[2, 1, -1]
-Output: 0
-Left sum = 0, right sum = 1 + (-1) = 0
+Return the number of leaf nodes in the tree.
 """
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
