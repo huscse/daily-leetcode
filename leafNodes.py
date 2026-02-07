bee6cf685d60082f27e31995aa84a5afb443745e
huscse <husnainkhaliq82@gmail.com>
2026-02-07 15:03:37 -0500

    solution

--- leafNodes.py
+++ leafNodes.py
@@ -12,9 +12,18 @@ no right child
 Return the number of leaf nodes in the tree.
 """
 
-
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+def countLeaves(root):
+    if root is None:
+        return 0
+    
+    # leaf check
+    if root.left is None and root.right is None:
+        return 1
+    
+    return countLeaves(root.left) + countLeaves(root.right)
