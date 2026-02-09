e5172fc09ec6bc0a648759cdbb1876fb33a25399
huscse <husnainkhaliq82@gmail.com>
2026-02-09 18:34:30 -0500

    nodes greater than x solution

--- countGreater.py
+++ countGreater.py
@@ -4,3 +4,14 @@
 Given a binary tree and an integer x,
 return the number of nodes whose value is strictly greater than x.
 """
+
+def countGreater(root, x):
+    if root is None:
+        return 0
+    
+    count = 0
+
+    if root.val > x:
+        count += 1
+
+    return count + countGreater(root.left) + countGreater(root.right)
