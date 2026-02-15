b92b1fabec3466c024917d37533cac18387d7111
huscse <husnainkhaliq82@gmail.com>
2026-02-14 19:01:27 -0500

    basic bfs structure

--- plain-bfs.py
+++ plain-bfs.py
@@ -1,3 +1,28 @@
 """
     02/14/2026 - BFS
 """
+
+from collections import deque
+
+def bfs(root):
+    if root is None:
+        return []
+    
+    result = []
+    
+    queue = deque([root])
+
+    while queue:
+        node = queue.popleft()
+        # process node here
+        result.append(node.val)
+
+        if node.left:
+            queue.append(node.left)
+
+        if node.right:
+            queue.append(node.right)
+
+    return result
+
+        
