9f030934cffd3f7c4b7f44dca0d12d8590479a3f
huscse <husnainkhaliq82@gmail.com>
2026-02-14 19:05:23 -0500

    level order bfs

--- plain-bfs.py
+++ plain-bfs.py
@@ -25,4 +25,31 @@ def bfs(root):
 
     return result
 
+
+def levelOrder(root):
+    if root is None:
+        return []
+    
+    result = []
+    queue = deque([root])
+
+    while queue:
+        level = []
+        level_size = len(queue)
+
+        for _ in range(level_size):
+            node = queue.popleft()
+            level.append(node.val)
+
+            if node.left:
+                queue.append(node.left)
+
+            if node.right:
+                queue.append(node.right)
+
+        result.append(level)
+
+    return result
+
+
         
